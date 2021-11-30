c311991e5854371df5489f07bbf27086ae86cffd
Ayo Alfonso <ayo@lynchpyn.com>
2021-11-30 08:18:26 +0100

    Hide Headers

--- pages/sso.ts
+++ pages/sso.ts
@@ -38,39 +38,42 @@ export const getServerSideProps: GetServerSideProps = async ({
   }
   let { sso, sig } = query;
   if (typeof sso !== "string") return { props: { error: true } };
+  try {
+    const hmac1 = crypto.createHmac(
+      "sha256",
+      process.env.NEXT_PUBLIC_DISCOURSE_SECRET || ""
+    );
+    hmac1.update(Buffer.from(sso));
+    let verifySig = hmac1.digest("hex");
+    if (verifySig !== sig) return { props: { error: true } };
 
-  const hmac1 = crypto.createHmac(
-    "sha256",
-    process.env.NEXT_PUBLIC_DISCOURSE_SECRET || ""
-  );
-  hmac1.update(Buffer.from(sso));
-  let verifySig = hmac1.digest("hex");
-  if (verifySig !== sig) return { props: { error: true } };
+    let { nonce } = querystring.parse(
+      Buffer.from(sso as string, "base64").toString()
+    );
 
-  let { nonce } = querystring.parse(
-    Buffer.from(sso as string, "base64").toString()
-  );
-
-  res.writeHead(301, {
-    Location:
+    res.redirect(
+      301,
       `${DISCOURSE_URL}/session/sso_login?` +
-      makeSSOPayload({
-        nonce: nonce as string,
-        email: token.email,
-        external_id: token.id,
-        username: token.username,
-      }),
-  });
-  await prisma.people.update({
-    where: { id: token.id },
-    data: {
+        makeSSOPayload({
+          nonce: nonce as string,
+          email: token.email,
+          external_id: token.id,
+          username: token.username,
+        })
+    );
+    await prisma.people.update({
+      where: { id: token.id },
+      data: {
+        classroom_onboarding: true,
+      },
+    });
+    setTokenHeader({
+      ...token,
       classroom_onboarding: true,
-    },
-  });
-  setTokenHeader({
-    ...token,
-    classroom_onboarding: true
-  });
-  res.end();
-  return { props: {} };
+    });
+    res.end();
+    return { props: {} };
+  } catch (error) {
+    console.log(error, "here");
+  }
 };
